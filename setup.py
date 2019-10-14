c41073a5ecad97875c835204e33f5f3be4224d37
David Cuthbert <dacut@kanga.org>
2019-10-14 00:18:52 -0700

    Bump version number.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="Manage the local user database from DynamoDB",
     long_description=readme,
     long_description_content_type="text/markdown",
-    version="1.0.0",
+    version="1.0.1",
     packages=["dynamodbusermanager"],
     entry_points={
         "console_scripts": [
